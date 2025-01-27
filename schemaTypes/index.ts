36271e0e675eafde1512b01f7092611aea773829
“Daniel <danielwdewald@gmail.com>
2025-01-27 13:25:54 -0500

    Manually pull in new document types from sanity podcast repo

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -4,5 +4,34 @@ import post from './post'
 import author from './author'
 import artwork from './artwork'
 import show from './show'
+import {
+  podcast,
+  episode,
+  itunes,
+  schedule,
+  linkListItem,
+  itunesEpisodeSettings,
+  sponsor,
+  sponsorRead,
+  owner,
+  categories,
+} from './lib/sanity-plugin-podcast/src'
 
-export const schemaTypes = [post, author, category, blockContent, artwork, show]
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  artwork,
+  show,
+  podcast,
+  episode,
+  itunes,
+  schedule,
+  linkListItem,
+  itunesEpisodeSettings,
+  sponsor,
+  sponsorRead,
+  owner,
+  categories,
+]
